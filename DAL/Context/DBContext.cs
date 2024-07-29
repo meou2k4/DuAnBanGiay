c5adb8d93cd42cfeab5dcc26930b2e508638438d
meou2k4 <nhathungvai@gmail.com>
2024-07-29 12:07:24 +0700

    Add project files.

--- DAL/Context/DBContext.cs
+++ DAL/Context/DBContext.cs
@@ -44,7 +44,7 @@ public partial class DBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=HHUNGDZ\\SQLEXPRESS;Initial Catalog=CuaHangBanHang_1;Integrated Security=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=MEO-U-LAPTOP;Initial Catalog=CuaHangBanHang_1;Integrated Security=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
